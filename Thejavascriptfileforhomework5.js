8f891d6f2d27d6ca1a0d3764b8389e85d46d5e0b
Christopher Beckom <christopher.beckom@gmail.com>
2013-03-18 00:30:44 -0700

    Homework 5

--- Thejavascriptfileforhomework5.js
+++ Thejavascriptfileforhomework5.js
@@ -8,60 +8,61 @@ var select = prompt("Please enter a number between 2 and 10:");
 if (select <= 11 && select >= 1) {
     document.write("<h2 />Your input number is " + [select])
 }
-else {
+ else {
     document.write("<h2 id='else' />Your input is " + [select] + ".") 
     document.write("The valid input number is between 2 and 10. Please reload this page and try again.<h2 />");
+    returns;
 }
 
-var entered = [select];
+    var entered = [select];
 
-var x = [entered];
+    var x = [entered];
 
-var count = [0.00];
+    var count = [0.00];
 
-while (x > .000001)
+    while (x > .000001)
 
-{
-    x /= 2;
-    count++;
-}
-{
-    document.write("<h2 />The number of times to divide the number " + [entered])
-    document.write(" by 2 to get a value less than one millionth is " + [entered] + [count]);
-}
+    {
+        x /= 2;
+        count++;
+    }
+    {
+        document.write("<h2 />The number of times to divide the number " + [entered])
+        document.write(" by 2 to get a value less than one millionth is " + [entered] + [count]);
+    }
 
-if ( select == 6) {
-    document.write('<ul>')
-    document.write("<br />******<br />", "<br />*****<br />", "<br />****<br />", "<br />***<br />", "<br />**<br />", "<br />*<br />")
-    document.write('</ul>');
-}
+    if ( select == 6) {
+        document.write('<ul>')
+        document.write("<br />******<br />", "<br />*****<br />", "<br />****<br />", "<br />***<br />", "<br />**<br />", "<br />*<br />")
+        document.write('</ul>');
+    }
 
-if (select == 5) {
-    document.write('<ul>')
-    document.write("<br />*****<br />", "<br />****<br />", "<br />***<br />", "<br />**<br />", "<br />*<br />")
-    document.write('</ul>');
-}
+    if (select == 5) {
+        document.write('<ul>')
+        document.write("<br />*****<br />", "<br />****<br />", "<br />***<br />", "<br />**<br />", "<br />*<br />")
+        document.write('</ul>');
+    }
 
-if (select == 4) {
-    document.write('<ul>')
-    document.write("<br />****<br />", "<br />***<br />", "<br />**<br />", "<br />*<br />")
-    document.write('</ul>');
-}
+    if (select == 4) {
+        document.write('<ul>')
+        document.write("<br />****<br />", "<br />***<br />", "<br />**<br />", "<br />*<br />")
+        document.write('</ul>');
+    }
 
-if (select == 3) {
-    document.write('<ul>')
-    document.write("<br />***<br />", "<br />**<br />", "<br />*<br />")
-    document.write('</ul>');
-}
+    if (select == 3) {
+        document.write('<ul>')
+        document.write("<br />***<br />", "<br />**<br />", "<br />*<br />")
+        document.write('</ul>');
+    }
 
-if (select == 2) {
-    document.write('<ul>')
-    document.write("<br />**<br />", "<br />*<br />")
-    document.write('</ul>');
-}
+    if (select == 2) {
+        document.write('<ul>')
+        document.write("<br />**<br />", "<br />*<br />")
+        document.write('</ul>');
+    }
 
-if (select == 1) {
-    document.write('<ul>')
-    document.write("<br />*<br />")
-    document.write('</ul>');
-}
+    if (select == 1) {
+        document.write('<ul>')
+        document.write("<br />*<br />")
+        document.write('</ul>');
+    }
